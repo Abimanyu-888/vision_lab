2c5cc21fda625918f85e73647f3d0b98d1480db7
Abimanyu-888 <abhimanyuchoudhary888@gmail.com>
2026-07-19 11:19:02 +0530

    mongodb address changed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({ path: './database.env' });
 export async function connectDB() {
     try{
-        await mongoose.connect("mongodb://127.0.0.1:27017/VisionLab")
+        await mongoose.connect(process.env.MONDODB_URI)
         .then(()=>console.log("Connected"))
     }
     catch(err){
